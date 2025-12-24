e9506d998a817aca90f6f628143bf85e1e65ab11
jayadeep05 <jayadeepvarada2004@gmail.com>
2025-12-24 23:38:11 +0530

    Update Vite proxy config and backend DB credentials

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: '/',
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:8080',
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-// })
-
-// for production
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -24,4 +5,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
+
+// for production
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/',
+//   plugins: [react()],
+// })
